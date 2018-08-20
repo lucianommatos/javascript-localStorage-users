ca48aab8ccd396040fa8786fd998455b8c54d4b4
Luciano Martins Matos <lucianommatos@gmail.com>
2018-08-21 00:15:27 +0100

    Adding project
    First commit

---
+++ plus/Util.js
@@ -0,0 +1,8 @@
+class Util {
+
+    static dateFormat(date) {
+        return date.getDate() + '/' + (date.getMonth() + 1) + '/' 
+            + date.getFullYear() + ' ' + date.getHours() + ':' + date.getMinutes();
+    }
+
+}
